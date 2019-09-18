5be5ddcab47da8f08a5a46c5cceb836096f420d2
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-09-18 03:54:20 -0700

    set up class and constructor, need to set up the methods

---
+++ bankAccount-challenge/bankAccount.ts
@@ -0,0 +1,17 @@
+class bankAccount {
+    owner: string;
+    balance: number;
+    transactions: array;
+
+    constructor(owner: string, balance: number, transactions: array) {
+        this.owner = owner;
+        this.balance = balance;
+        this.transactions = transactions;
+    }
+
+    getBalance()
+
+    withdrawal()
+
+    deposit()
+}
